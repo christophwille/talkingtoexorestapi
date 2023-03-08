5aaa7b4e911e08f10b23e798734c154dbabe1a7b
Christoph Wille <christoph.wille@gmail.com>
2023-03-08 15:45:18 +0100

    Add LoadFromResourceCached

--- src/AdminApiClient.For.ExchangeOnline/ExOMetadata.cs
+++ src/AdminApiClient.For.ExchangeOnline/ExOMetadata.cs
@@ -1,20 +1,38 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Text;
 
 namespace AdminApiClient.For.ExchangeOnline
 {
     public static class ExOMetadata
     {
+        private static readonly string ResourceName = "AdminApiClient.For.ExchangeOnline.Resource.metadata.xml";
+
         public static async Task<string> LoadFromResourceAsync()
         {
             var assembly = typeof(ExOMetadata).GetTypeInfo().Assembly;
-            var resourceStream = assembly.GetManifestResourceStream("AdminApiClient.For.ExchangeOnline.Resource.metadata.xml");
+            var resourceStream = assembly.GetManifestResourceStream(ResourceName);
 
             using (var reader = new StreamReader(resourceStream, Encoding.UTF8))
             {
                 return await reader.ReadToEndAsync();
             }
+        }
+
+
+        private static string? metadataDocument = null;
+        public static string LoadFromResourceCached()
+        {
+            if (null != metadataDocument) return metadataDocument;
+
+            var assembly = typeof(ExOMetadata).GetTypeInfo().Assembly;
+            var resourceStream = assembly.GetManifestResourceStream(ResourceName);
+
+            using (var reader = new StreamReader(resourceStream, Encoding.UTF8))
+            {
+                metadataDocument = reader.ReadToEnd();
+            }
+
+            return metadataDocument;
         }
     }
 }
